04e735972655bf1dceba30debe699401f5d58f0b
answerywj <1045837697@qq.com>
2019-06-03 17:15:46 +0800

    create source file

---
+++ wavfile.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: wavfile.h
+	> Author: answer
+	> Mail: yuanweijie1993@gmail.com
+	> Created Time: Mon 03 Jun 2019 05:15:29 PM CST
+ ************************************************************************/
+
+#ifndef __WAVFILE__H
+#define __WAVFILE__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
